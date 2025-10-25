64a12ebad77329720f3495be82b2f7d14703e20e
Pratik Khaire <Pratik.shekhar.khaire@gmail.com>
2025-10-26 00:22:29 +0530

    feat: integrate react-three for 3D product visualization
    - Added dependencies for @react-three/fiber and @react-three/drei to package.json.
    - Updated ProductViewer component to include a 3D canvas using react-three.
    - Implemented a Box component to represent the Macbook with dynamic scaling and color based on user selection.

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import useMacbookStore from '../store'
 import clsx from 'clsx';
+import { Canvas } from '@react-three/fiber';
+import { Box } from '@react-three/drei';
 
 const ProductViewer = () => {
 
@@ -9,7 +11,7 @@ const ProductViewer = () => {
     <section id='product-viewer'>
         <h2>Take a closer look</h2>
         <div className=' controls'>
-            <p className=' info'> MacbookPro 16" in  {color} </p>
+            <p className=' info'> MacbookPro {scale}" in  {color} </p>
             <div className=' flex-center gap-5 mt-5'>
                 <div className="color-control">
                     <div onClick={()=> setColor('#adb5bd')} 
@@ -19,11 +21,13 @@ const ProductViewer = () => {
                     
                 </div>
                 <div className="size-control">
-                     <div onClick={()=> setScale('0.06')} 
+
+
+                     <div onClick={()=> setScale(0.06)} 
                     className={clsx(scale=== 0.06 ? 'bg-white text-black' : 'bg-transparent text-white')}  >
                         <p>14"</p>
                     </div>
-                     <div onClick={()=> setScale('0.08')} 
+                     <div onClick={()=> setScale(0.08)} 
                     className={clsx(scale=== 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')}  >
                         <p>16"</p>
                     </div>
@@ -34,7 +38,10 @@ const ProductViewer = () => {
         </div>
         
      <p className=' text-white text-4xl '> Render Canvas</p>
-        
+
+        <Canvas id='canvas' camera={ { position:[0,2,5], fov:50,near:0.1,far:100}}>
+  <Box  position={[0,1,0]} scale={15 * scale} material-color={color}/>
+        </Canvas>
     </section>
   )
 }
