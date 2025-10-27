4d60b3ef55c87d66361933c0e54417c61c877579
Pratik Khaire <Pratik.shekhar.khaire@gmail.com>
2025-10-27 23:36:05 +0530

    feat: enhance Features and Macbook components with GSAP animations and video preloading

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import { texture } from "three/tsl";
 import { create } from "zustand";
 const useMacbookStore = create((set) => ({
     color:'#2e2c2e',
@@ -6,7 +7,10 @@ const useMacbookStore = create((set) => ({
     scale:0.08,
     setScale:(scale) => set({scale}),
 
-    reset:() => set({ color:'#2e2c3e', scale:0.08}),
+    texture:'/videos/feature-1.mp4',
+    setTexture: (texture)=> set({texture}),
+
+    reset:() => set({ color:'#2e2c3e', scale:0.08 ,texture:'/video/feature-1.mp4'}),
 }))
 
 
